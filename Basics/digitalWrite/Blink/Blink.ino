fbee8e5905e4a67e055ad5f0865dbba5e11dd2f0
Jon Froehlich <jonf@cs.uw.edu>
2020-03-16 10:16:10 -0700

    Update Blink.ino

--- Basics/digitalWrite/Blink/Blink.ino
+++ Basics/digitalWrite/Blink/Blink.ino
@@ -1,19 +1,25 @@
 /*
  * Simply turns on and off pin 3 once a second
  * 
- * By Jon Froehlich
+ * By Jon #. Froehlich
+ * @jonfroehlich
  * http://makeabilitylab.io
+ * 
+ * Example Tinkcard circuit:
+ *  - https://www.tinkercad.com/things/3EzaisGSd0c-blink-led-onoff
  * 
  * Adapted from the official Arduino Blink example:
  *   File -> Examples -> 01. Basics -> Blink
  */
 
+// While not necessary, it's a good idea to use const variables
+// for constants like specifying pins that you use.
 const int LED_OUTPUT_PIN = 3;
 
 // The setup function runs once when you press reset or power the board
 void setup() {
-  // Because pins 0 - 13 can either be input or output, we must specify
-  // how we're using the pin by using pinMode. In this case, we want to
+  // On the Arduino Uno, Leonardo, etc. pins 0 - 13 can either be input or output.
+  // We must specify how we're using the pin by using pinMode. In this case, we want to
   // control an LED, so set the pin to OUTPUT
   pinMode(LED_OUTPUT_PIN, OUTPUT);
 }
