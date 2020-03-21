35f88f20507c5aa35efef7c5c40735267b4573b5
Jon Froehlich <jonfroehlich@gmail.com>
2020-03-20 19:33:22 -0700

    added delay constant to Blink2LEDs

--- Basics/digitalWrite/Blink2LEDs/Blink2LEDs.ino
+++ Basics/digitalWrite/Blink2LEDs/Blink2LEDs.ino
@@ -18,6 +18,7 @@
 
 const int LED1_OUTPUT_PIN = 3; // Anode faces Pin 3 (cathode connected to 0V)
 const int LED2_OUTPUT_PIN = 4; // Cathode faces Pin 4 (anode connected to 5V)
+const int DELAY_MS = 1000; // delay for 1 sec between blinks
 
 // The setup function runs once when you press reset or power the board
 void setup() {
@@ -38,11 +39,11 @@ void loop() {
   digitalWrite(LED1_OUTPUT_PIN, HIGH);  // turns ON LED1 (Pin 3 is now 5V and other leg of LED is 0V)
   digitalWrite(LED2_OUTPUT_PIN, HIGH);  // turns OFF LED2 (Pin 4 is now 5V and other leg of LED is 5V)
   Serial.println("Writing out HIGH; LED1 should be ON, LED2 OFF");
-  delay(1000);                          // delay is in milliseconds; so wait one second
+  delay(DELAY_MS);                          // delay is in milliseconds; so wait one second
   
   
   digitalWrite(LED1_OUTPUT_PIN, LOW);   // turns OFF LED1 (Pin 3 is now 0V and other leg of LED is 0V)
   digitalWrite(LED2_OUTPUT_PIN, LOW);   // turns ON LED2 (Pin 4 is now 0V and other leg of LED is 5V)
   Serial.println("Writing out LOW; LED1 should be OFF, LED2 ON");
-  delay(1000);                          // wait for a second
+  delay(DELAY_MS);                          // wait for a second
 }
