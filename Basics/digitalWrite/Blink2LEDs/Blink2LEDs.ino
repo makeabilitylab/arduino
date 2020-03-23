3e39b49fbf2fdadf01c64f0d2127024e802e7622
Jon Froehlich <jonfroehlich@gmail.com>
2020-03-23 09:12:43 -0700

    updated comments for Blink2LEDs

--- Basics/digitalWrite/Blink2LEDs/Blink2LEDs.ino
+++ Basics/digitalWrite/Blink2LEDs/Blink2LEDs.ino
@@ -26,9 +26,7 @@ const int DELAY_MS = 1000; // delay for 1 sec between blinks
 
 // The setup function runs once when you press reset or power the board
 void setup() {
-  // Because pins 0 - 13 can either be input or output, we must specify
-  // how we're using the pin by using pinMode. In this case, we want to
-  // control LEDs, so set both pins to OUTPUT
+  // Set our LED pins as output
   pinMode(LED1_OUTPUT_PIN, OUTPUT);
   pinMode(LED2_OUTPUT_PIN, OUTPUT);
 
@@ -40,14 +38,13 @@ void setup() {
 void loop() {
   // Below, you're going to see that driving Pin 3 HIGH will turn on LED1
   // but driving Pin 4 HIGH will actually turn *off* LED2
-  digitalWrite(LED1_OUTPUT_PIN, HIGH);  // turns ON LED1 (Pin 3 is now 5V and other leg of LED is 0V)
-  digitalWrite(LED2_OUTPUT_PIN, HIGH);  // turns OFF LED2 (Pin 4 is now 5V and other leg of LED is 5V)
+  digitalWrite(LED1_OUTPUT_PIN, HIGH);  // turns ON LED1
+  digitalWrite(LED2_OUTPUT_PIN, HIGH);  // turns OFF LED2
   Serial.println("Writing out HIGH; LED1 should be ON, LED2 OFF");
-  delay(DELAY_MS);                          // delay is in milliseconds; so wait one second
-  
+  delay(DELAY_MS);                      // delay is in milliseconds; so wait one second
   
   digitalWrite(LED1_OUTPUT_PIN, LOW);   // turns OFF LED1 (Pin 3 is now 0V and other leg of LED is 0V)
   digitalWrite(LED2_OUTPUT_PIN, LOW);   // turns ON LED2 (Pin 4 is now 0V and other leg of LED is 5V)
   Serial.println("Writing out LOW; LED1 should be OFF, LED2 ON");
-  delay(DELAY_MS);                          // wait for a second
+  delay(DELAY_MS);                      // wait for a second
 }
