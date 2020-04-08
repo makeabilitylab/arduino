f9328c96aa71417a4e8e2a52639fc6a10b184fc6
Jon Froehlich <jonfroehlich@gmail.com>
2020-04-08 05:38:40 -0700

    Update BlinkRGBSimple.ino

--- Basics/digitalWrite/BlinkRGBSimple/BlinkRGBSimple.ino
+++ Basics/digitalWrite/BlinkRGBSimple/BlinkRGBSimple.ino
@@ -1,15 +1,28 @@
 /*
- * This example changes the colors of the RGB LED using digitalWrite
+ * This example flashes through a predefined sequence of colors 
+ * using an RGB LED and digitalWrite. Make sure to correctly set
+ * IS_COMMON_ANODE to true (if you're using a common anode RGB LED)
+ * or to false (if you're using a common cathode RGB LED).
  * 
  * By Jon Froehlich
+ * @jonfroehlich
  * http://makeabilitylab.io
+ * 
+ * For a walkthrough and circuit diagram, see:
+ * https://makeabilitylab.github.io/physcomp/arduino/rgb-led
  * 
  */
 
+// Change this to based on whether you are using a common anode or common cathode
+// RGB LED. See: https://makeabilitylab.github.io/physcomp/arduino/rgb-led 
+// If you are working with a common cathode RGB LED, set this to false.
+// Note Adafruit sells a common anode design: https://www.adafruit.com/product/159
+const boolean IS_COMMON_ANODE = false; 
+
 const int RGB_RED_PIN = 6;
-const int RGB_GREEN_PIN  = 5;
-const int RGB_BLUE_PIN  = 3;
-const int DELAY = 1000; // delay in ms between changing colors
+const int RGB_GREEN_PIN = 5;
+const int RGB_BLUE_PIN = 3;
+const int DELAY_MS = 1000; // delay in ms between changing colors
 
 void setup() {
   // Set the RGB pins to output
@@ -23,43 +36,64 @@ void setup() {
 
 void loop() {
 
-  // Because the RGB LED we purchased for the class is a 'common anode'
-  // RGB, the way we turn on each light is counter to our intuition
-  // We set a pin to LOW to turn on the corresponding LED to its maximum brightness
-  // and HIGH to turn it off (the opposite of what you might think)
+  // red
+  Serial.print("Red: ");
+  setRgbLedColor(HIGH, LOW, LOW);
+  delay(DELAY_MS);
+  
+  // green
+  Serial.print("Green: ");
+  setRgbLedColor(LOW, HIGH, LOW);
+  delay(DELAY_MS);
+  
+  // blue
+  Serial.print("Blue: ");
+  setRgbLedColor(LOW, LOW, HIGH);
+  delay(DELAY_MS);
+  
+  // purple
+  Serial.print("Purple: ");
+  setRgbLedColor(HIGH, LOW, HIGH);
+  delay(DELAY_MS);
+  
+  // turqoise
+  Serial.print("Turqoise: ");
+  setRgbLedColor(LOW, HIGH, HIGH);
+  delay(DELAY_MS);
+  
+  // yellow
+  Serial.print("Yellow: ");
+  setRgbLedColor(HIGH, HIGH, LOW);
+  delay(DELAY_MS);
+  
+  // white
+  Serial.print("White: ");
+  setRgbLedColor(HIGH, HIGH, HIGH);
+  delay(DELAY_MS);
+}
 
-  // Set the RGB LED to red
-  Serial.println("Color=Red (LOW, HIGH, HIGH)");
-  digitalWrite(RGB_RED_PIN, LOW);     // turn on the red LED
-  digitalWrite(RGB_GREEN_PIN, HIGH);  // turn off the green LED
-  digitalWrite(RGB_BLUE_PIN, HIGH);   // turn off the blue LED
-  delay(DELAY);
 
-  // Set the RGB LED to green
-  Serial.println("Color=Green (HIGH, LOW, HIGH)");
-  digitalWrite(RGB_RED_PIN, HIGH);    // turn off the red LED
-  digitalWrite(RGB_GREEN_PIN, LOW);   // turn on the green LED
-  digitalWrite(RGB_BLUE_PIN, HIGH);   // turn on the blue LED
-  delay(DELAY);
+/**
+ * setRgbLedColor takes in either HIGH or LOW for red, green, and blue.
+ * 
+ * This function is based on https://gist.github.com/jamesotron/766994
+ */
+void setRgbLedColor(int red, int green, int blue)
+{
+  // If a common anode LED, invert values
+  if(IS_COMMON_ANODE == true){
+    red = !red;
+    green = !green;
+    blue = !blue;
+  }
 
-  // Set the RGB LED to blue
-  Serial.println("Color=Blue (HIGH, HIGH, LOW)");
-  digitalWrite(RGB_RED_PIN, HIGH);    // turn off the red LED
-  digitalWrite(RGB_GREEN_PIN, HIGH);  // turn off the green LED
-  digitalWrite(RGB_BLUE_PIN, LOW);    // turn on the blue LED
-  delay(DELAY);
-
-  // Set the RGB LED to yellow (by turning on green and blue!)
-  Serial.println("Color=Yellow (255, LOW, LOW)");
-  digitalWrite(RGB_RED_PIN, HIGH);    // turn off the red LED
-  digitalWrite(RGB_GREEN_PIN, LOW);   // turn on the green LED
-  digitalWrite(RGB_BLUE_PIN, LOW);    // turn on the blue LED
-  delay(DELAY);
-
-  // Set the RGB LED to purple (by turning on red and blue!)
-  Serial.println("Color=Purple (LOW, HIGH, LOW)");
-  digitalWrite(RGB_RED_PIN, LOW);     // turn on the red LED
-  digitalWrite(RGB_GREEN_PIN, HIGH);  // turn off the green LED
-  digitalWrite(RGB_BLUE_PIN, LOW);    // turn on the blue LED
-  delay(DELAY);
+  Serial.print(red);
+  Serial.print(", ");
+  Serial.print(green);
+  Serial.print(", ");
+  Serial.println(blue);
+  
+  digitalWrite(RGB_RED_PIN, red);
+  digitalWrite(RGB_GREEN_PIN, green);
+  digitalWrite(RGB_BLUE_PIN, blue);  
 }
