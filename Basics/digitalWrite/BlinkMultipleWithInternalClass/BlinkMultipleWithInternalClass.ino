25ad77aed81ef16d80a420645eb3d7b07e28fa33
Jon Froehlich <jonfroehlich@gmail.com>
2020-04-13 16:47:27 -0700

    updated comment

--- Basics/digitalWrite/BlinkMultipleWithInternalClass/BlinkMultipleWithInternalClass.ino
+++ Basics/digitalWrite/BlinkMultipleWithInternalClass/BlinkMultipleWithInternalClass.ino
@@ -62,9 +62,9 @@ class Blinker{
     }
 };
 
-Blinker _led1Blinker(2, 200);  // specify pin, blink interval
-Blinker _led2Blinker(5, 333);  // specify pin, blink interval
-Blinker _led3Blinker(9, 1111); // specify pin, blink interval
+Blinker _led1Blinker(2, 200);  // specify pin and blink interval (200ms)
+Blinker _led2Blinker(5, 333);  // specify pin and blink interval (333ms)
+Blinker _led3Blinker(9, 1111); // specify pin and blink interval (1111ms)
 
 // The setup function runs once when you press reset or power the board
 void setup() {
