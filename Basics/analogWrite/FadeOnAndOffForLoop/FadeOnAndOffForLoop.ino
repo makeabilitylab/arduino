b5537558f8fcafe594158e61b07f51fb6bd865d9
Jon Froehlich <jonfroehlich@gmail.com>
2020-04-13 05:45:15 -0700

    added constant MAX_ANALOG_OUT

--- Basics/analogWrite/FadeOnAndOffForLoop/FadeOnAndOffForLoop.ino
+++ Basics/analogWrite/FadeOnAndOffForLoop/FadeOnAndOffForLoop.ino
@@ -13,6 +13,7 @@
  */
 const int LED_OUTPUT_PIN = 3;
 const int DELAY_MS = 5; // delay between each fade value
+const int MAX_ANALOG_OUT = 255; // the max analog output on the Uno is 255
 
 void setup() {
   // set the LED pin to an output
@@ -21,13 +22,13 @@ void setup() {
 
 void loop(){
   // fade on
-  for(int i = 0; i <= 255; i += 1){
+  for(int i = 0; i <= MAX_ANALOG_OUT; i += 1){
     analogWrite(LED_OUTPUT_PIN, i);
     delay(DELAY_MS);
   }
 
   //fade off
-  for(int i = 255; i >= 0; i -= 1){
+  for(int i = MAX_ANALOG_OUT; i >= 0; i -= 1){
     analogWrite(LED_OUTPUT_PIN, i);
     delay(DELAY_MS);
   }
