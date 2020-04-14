dfb671e47a7915e059bf87af8e3a55656bcf5baa
Jon Froehlich <jonfroehlich@gmail.com>
2020-04-14 13:03:58 -0700

    Create SimpleSiren.ino

---
+++ Basics/digitalRead/SimpleSiren/SimpleSiren.ino
@@ -0,0 +1,36 @@
+/*
+ * Demonstrates how to make a simple siren using a piezo
+ * 
+ * By Jon E. Froehlich
+ * @jonfroehlich
+ * http://makeabilitylab.io
+ * 
+ * For a walkthrough and circuit diagram, see:
+ * https://makeabilitylab.github.io/physcomp/arduino/piano
+ * 
+ * Note: Use of the tone() function will interfere with PWM output on pins 3 and 11 
+ * (on boards other than the Mega). See:
+ * https://www.arduino.cc/reference/en/language/functions/advanced-io/tone/
+ * 
+ */
+const int OUTPUT_PIEZO_PIN = 9;
+const int OUTPUT_LED_PIN = LED_BUILTIN;
+const int SOUND_DURATION_MS = 500;
+
+void setup() {
+  pinMode(OUTPUT_PIEZO_PIN, OUTPUT);
+  pinMode(OUTPUT_LED_PIN, OUTPUT);
+}
+
+void loop() {
+  
+  // tone() generates a square wave of the specified frequency 
+  // (and 50% duty cycle) on a pin. 
+  tone(OUTPUT_PIEZO_PIN, 392);
+  digitalWrite(OUTPUT_LED_PIN, HIGH);
+  delay(SOUND_DURATION_MS);
+  
+  tone(OUTPUT_PIEZO_PIN, 262);
+  digitalWrite(OUTPUT_LED_PIN, LOW);
+  delay(SOUND_DURATION_MS);
+}
