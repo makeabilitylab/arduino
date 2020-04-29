0bbe9bbe6f75c8370246dae77e8ae40c4b939b8b
Jon Froehlich <jonfroehlich@gmail.com>
2020-04-29 13:44:04 -0700

    Update AnalogInputTest.ino

--- ESP32/Basics/AnalogInputTest/AnalogInputTest.ino
+++ ESP32/Basics/AnalogInputTest/AnalogInputTest.ino
@@ -1,7 +1,25 @@
+/**
+ * This example reads all of the analog input pins on the Huzzah32 and prints
+ * their values to Serial. The official Adafruit Huzzah32 docs state that
+ * ADC#1 only works with WiFi has started (bottom of page):
+ * https://learn.adafruit.com/adafruit-huzzah32-esp32-feather/pinouts
+ * 
+ * However, we used this program to show that this is not true.
+ * 
+ * By Jon E. Froehlich
+ * @jonfroehlich
+ * http://makeabilitylab.io
+ * 
+ * See our ESP32 guide here:
+ * https://makeabilitylab.github.io/physcomp/esp32/
+ * 
+ */
 
 const int GPIO_PINS[] = {26, 25, 4, 5, 18, 19, 16, 17, 21, 13, 12, 27, 33, 15, 32, 14, 22, 23};
 
 // For some reason, A12 is not defined. So, using raw literal 13 instead
+// ADC#1 includes A2, A3, A4, A7, A9
+// ADC#2 includes A0, A1, A5, A6, A8, A10, A11, and A12
 const int ANALOG_INPUT_PINS[] = { A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, 13 };
 
 int _numAnalogInputPins = 0;
