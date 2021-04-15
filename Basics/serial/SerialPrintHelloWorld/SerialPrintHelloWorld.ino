57890538e3f5a5031bb6081d3e32c0d728e85e16
Jon Froehlich <jonfroehlich@gmail.com>
2021-04-15 14:15:45 -0700

    added comments

--- Basics/serial/SerialPrintHelloWorld/SerialPrintHelloWorld.ino
+++ Basics/serial/SerialPrintHelloWorld/SerialPrintHelloWorld.ino
@@ -1,8 +1,25 @@
+/*
+ * Simplest possible example to demonstrate use of serial protocol.
+ * 
+ * References
+ * - https://www.arduino.cc/en/reference/serial
+ * 
+ * By Jon E. Froehlich
+ * @jonfroehlich
+ * http://makeabilitylab.io
+ * 
+ */
 void setup() {
+  // Initialize the serial port
+  // https://www.arduino.cc/reference/en/language/functions/communication/serial/begin/
   Serial.begin(9600); // opens serial port, sets data rate to 9600 bps
 }
 
 void loop() {
+  // Print out "Hello world!" 
+  // https://www.arduino.cc/reference/en/language/functions/communication/serial/println/
   Serial.println("Hello world!");
+
+  // Delay by 500ms
   delay(500);
 }
