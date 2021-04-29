dc0460fa57bb406c7a6bc327be89c3e6c1b1c8c0
Jon Froehlich <jonfroehlich@gmail.com>
2021-04-29 09:18:35 -0700

    added comments

--- OLED/DrawCircle/DrawCircle.ino
+++ OLED/DrawCircle/DrawCircle.ino
@@ -1,3 +1,17 @@
+/**
+ * Simple example that animates an increasingly large circle on screen
+ * 
+ * Adafruit Gfx Library:
+ * https://learn.adafruit.com/adafruit-gfx-graphics-library/overview 
+ *
+ * Adafruit OLED tutorials:
+ * https://learn.adafruit.com/monochrome-oled-breakouts
+ *  
+ * By Jon E. Froehlich
+ * @jonfroehlich
+ * http://makeabilitylab.io
+ *
+ */
 
 #include <SPI.h>
 #include <Wire.h>
@@ -9,28 +23,29 @@
 
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
 #define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+Adafruit_SSD1306 _display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 void setup() {
   Serial.begin(9600);
 
   // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
-  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3D)) { // Address 0x3D for 128x64
+  if(!_display.begin(SSD1306_SWITCHCAPVCC, 0x3D)) { // Address 0x3D for 128x64
     Serial.println(F("SSD1306 allocation failed"));
     for(;;); // Don't proceed, loop forever
   }
   
-  display.clearDisplay();
+  _display.clearDisplay();
 }
 
 void loop() {
   // Clear the buffer
-  display.clearDisplay();
-  
-  for(int16_t i=0; i<max(display.width(),display.height())/2; i+=2) {
-    //display.drawCircle(display.width()/2, display.height()/2, i, SSD1306_WHITE);
-    display.fillCircle(display.width() / 2, display.height() / 2, i, SSD1306_WHITE);
-    display.display();
+  _display.clearDisplay();
+
+  // We could certainly modify this to not use an embedded loop
+  // But fine for now just to demonstrate drawing function
+  for(int16_t i=0; i<max(_display.width(),_display.height())/2; i+=2) {
+    _display.fillCircle(_display.width() / 2, _display.height() / 2, i, SSD1306_WHITE);
+    _display.display();
     delay(1);
   }
 
