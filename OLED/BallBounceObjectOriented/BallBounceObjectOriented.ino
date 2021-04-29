5c2a1fc1bcf61d5ffa3c8f6e8c2b2eccedab8843
Jon Froehlich <jonfroehlich@gmail.com>
2021-04-29 08:39:36 -0700

    added comments and now using Makeability Lab library for Ball

--- OLED/BallBounceObjectOriented/BallBounceObjectOriented.ino
+++ OLED/BallBounceObjectOriented/BallBounceObjectOriented.ino
@@ -1,25 +1,31 @@
 
 /**
- *  Draws a bouncing ball on the OLED display
- *
- *  Based on: https://makeabilitylab.github.io/p5js/Animation/BallBounce2D
- *  Source: https://github.com/makeabilitylab/p5js/tree/master/Animation/BallBounce2D
- *
- *  Adafruit Gfx Library:
- *  https://learn.adafruit.com/adafruit-gfx-graphics-library/overview 
- *
- *  Adafruit OLED tutorials:
- *  https://learn.adafruit.com/monochrome-oled-breakouts
+ * Draws a bouncing ball on the OLED display using the Ball class
  *  
- *  By Jon E. Froehlich
- *  @jonfroehlich
- *  http://makeabilitylab.io
+ * This code requires the Shape.hpp file from the MakeabilityLab_Arduino_Library
+ * 
+ * To install and use this library:
+ * 1. Get it from here: https://github.com/makeabilitylab/arduino/tree/master/MakeabilityLab_Arduino_Library
+ * 2. Move the entire folder to the Arduino libraries folder on your computer.
+ *   - On my Windows box, this is C:\Users\jonfr\Documents\Arduino\libraries
+ *   - On Mac, it's: /Users/jonf/Documents/Arduino/libraries
+ * 3. Then include the relevant libraries via #include <libraryname.h> or <libraryname.hpp>
+ *
+ * Adafruit Gfx Library:
+ * https://learn.adafruit.com/adafruit-gfx-graphics-library/overview 
+ *
+ * Adafruit OLED tutorials:
+ * https://learn.adafruit.com/monochrome-oled-breakouts
+ *  
+ * By Jon E. Froehlich
+ * @jonfroehlich
+ * http://makeabilitylab.io
  *
  */
 
 #include <Wire.h>
 #include <SPI.h>
-#include "Shape.hpp"
+#include <Shape.hpp>
 
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
@@ -37,15 +43,61 @@ const int DELAY_LOOP_MS = 5;
 Ball _ball(20, 20, 5);  
 
 // for tracking fps
-unsigned long _totalFrameCount = 0;
-unsigned long _startTimeStamp = 0;
+float _fps = 0;
+unsigned long _frameCount = 0;
+unsigned long _fpsStartTimeStamp = 0;
 
 // status bar
 const boolean _drawStatusBar = true; // change to show/hide status bar
 
 void setup() {
   Serial.begin(9600);
+  initializeOledAndShowStartupScreen();
 
+  // Gets a random long between min and max - 1
+  // https://www.arduino.cc/reference/en/language/functions/random-numbers/random/
+  int xSpeed = random(1, 4);
+  int ySpeed = random(1, 4);
+  _ball.setSpeed(xSpeed, ySpeed);
+  
+  _fpsStartTimeStamp = millis();
+}
+
+void loop() {
+  
+  _display.clearDisplay();
+  
+  if(_drawStatusBar){
+    drawStatusBar();
+  }
+
+  calcFrameRate();
+  
+  // Update ball based on speed and location
+  _ball.update();
+
+  if(_ball.checkXBounce(0, _display.width())){
+    _ball.reverseXSpeed();
+  }
+
+  if(_ball.checkYBounce(0, _display.height())){
+    _ball.reverseYSpeed();
+  }
+
+  _ball.draw(_display);
+
+  // Render buffer to screen
+  _display.display();
+
+  if(DELAY_LOOP_MS > 0){
+    delay(DELAY_LOOP_MS);
+  }
+}
+
+/**
+ * Call this from setup() to initialize the OLED screen
+ */
+void initializeOledAndShowStartupScreen(){
   // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
   if (!_display.begin(SSD1306_SWITCHCAPVCC, 0x3D)) { // Address 0x3D for 128x64
     Serial.println(F("SSD1306 allocation failed"));
@@ -64,42 +116,29 @@ void setup() {
   _display.clearDisplay();
 }
 
-void loop() {
-  if(_startTimeStamp == 0){
-    _startTimeStamp = millis();
+/**
+ * Call this every frame to calculate frame rate
+ */
+void calcFrameRate() {
+  unsigned long elapsedTime = millis() - _fpsStartTimeStamp;
+  _frameCount++;
+  if (elapsedTime > 1000) {
+    _fps = _frameCount / (elapsedTime / 1000.0);
+    _fpsStartTimeStamp = millis();
+    _frameCount = 0;
   }
+}
 
-  _display.clearDisplay();
-  
-  if(_drawStatusBar && _totalFrameCount > 0){
-    int16_t x1, y1;
-    uint16_t w, h;
-    unsigned long elapsedTime = millis() - _startTimeStamp;
-    float fps = _totalFrameCount / (elapsedTime / 1000.0);
-    _display.getTextBounds("XX.XX fps", 0, 0, &x1, &y1, &w, &h);
-    _display.setCursor(_display.width() - w, 0);
-    _display.print(fps);
-    _display.print(" fps");
-  }
-  
-  // Update ball based on speed location
-  _ball.update();
+/**
+ * Draws the status bar at top of screen with fps
+ */
+void drawStatusBar() {
 
-  if(_ball.checkXBounce(0, _display.width())){
-    _ball.reverseX();
-  }
-
-  if(_ball.checkYBounce(0, _display.height())){
-    _ball.reverseY();
-  }
-
-  _ball.draw(_display);
-
-  // Render buffer to screen
-  _display.display();
-  _totalFrameCount++;
-
-  if(DELAY_LOOP_MS > 0){
-    delay(DELAY_LOOP_MS);
-  }
+  // Draw frame count
+  int16_t x1, y1;
+  uint16_t w, h;
+  _display.getTextBounds("XX.XX fps", 0, 0, &x1, &y1, &w, &h);
+  _display.setCursor(_display.width() - w, 0);
+  _display.print(_fps);
+  _display.print(" fps");
 }
