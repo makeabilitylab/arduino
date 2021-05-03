767a8e8b1cf8aa4928dc6703670409e0e119bfc1
Jon Froehlich <jonfroehlich@gmail.com>
2021-05-03 05:15:22 -0700

    added param to check when moving average filter window is full

--- MakeabilityLab_Arduino_Library/src/MovingAverageFilter.hpp
+++ MakeabilityLab_Arduino_Library/src/MovingAverageFilter.hpp
@@ -4,6 +4,7 @@ class MovingAverageFilter{
     int _windowSize = -1;
     long _sampleTotal = 0; 
     int _curReadIndex = 0; 
+    bool _isWindowFull = false; // useful for knowing if the buffer is full or not
 
   public:
   /**
@@ -52,14 +53,38 @@ class MovingAverageFilter{
       if (_curReadIndex >= _windowSize) {
         // ...wrap around to the beginning:
         _curReadIndex = 0;
+        _isWindowFull = true;
       }  
     }
 
+    /**
+     * @brief Gets the average as an integer. If the division results in a fraction,
+     * the fractional component is discarded (truncated). No rounding occurs.
+     * 
+     * @return int the average as an integer (fractional components are discarded)
+     */
     int getAverage(){
       return _sampleTotal / _windowSize;
     }
   
+    /**
+     * @brief Get the average as an integer
+     * 
+     * @return float the average as a float
+     */
     float getAverageAsFloat(){
       return _sampleTotal / (float)_windowSize;
     }    
+
+    /**
+     * @brief Returns true if the sliding window is full of data. Only relevant when
+     * the algorithm first starts. This is helpful if you only want to use the getAverage() 
+     * result if the full sliding window state has been reached
+     * 
+     * @return true If the sliding window buffer is full
+     * @return false If the sliding window buffer is not full
+     */
+    bool isWindowFull(){
+        return _isWindowFull;
+    }
 };
