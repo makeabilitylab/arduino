837cc5900d8445bdfb539475720f19ef6dfa745a
Jon Froehlich <jonfroehlich@gmail.com>
2021-05-18 16:57:17 -0700

    added in demo that takes in two analog vals and prints normalized versions onto serial

---
+++ Serial/XYAnalogOut/XYAnalogOut.ino
@@ -0,0 +1,114 @@
+
+/**
+ *  Read in two analog inputs, translate them to X,Y float positions
+ *  between [0, 1] inclusive, and send over Serial
+ *
+ *  Adafruit Gfx Library:
+ *  https://learn.adafruit.com/adafruit-gfx-graphics-library/overview 
+ *
+ *  Adafruit OLED tutorials:
+ *  https://learn.adafruit.com/monochrome-oled-breakouts
+ *
+ *  By Jon E. Froehlich
+ *  @jonfroehlich
+ *  http://makeabilitylab.io
+ *
+ */
+
+#include <Wire.h>
+#include <SPI.h>
+
+
+// For graphics libraries
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+#define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
+Adafruit_SSD1306 _display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+const int DELAY_LOOP_MS = 10; 
+const int ANALOG_INPUT1_PIN = A0;
+const int ANALOG_INPUT2_PIN = A0;
+const int MAX_ANALOG_VAL = 1023;
+
+float _x;
+float _y;
+int _radius = 4;
+
+void setup() {
+  
+  Serial.begin(115200);
+
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if (!_display.begin(SSD1306_SWITCHCAPVCC, 0x3D)) { // Address 0x3D for 128x64
+    Serial.println(F("SSD1306 allocation failed"));
+    for (;;); // Don't proceed, loop forever
+  }
+
+  // Clear the buffer
+  _display.clearDisplay();
+  _display.setTextSize(1);
+  _display.setTextColor(WHITE, BLACK);
+  _display.setCursor(0, 0);
+  _display.println("Screen initialized!");
+  _display.display();
+  delay(250);
+
+  _x = _display.width() / 2;
+  _y = _display.height() / 2;
+}
+
+void loop() {
+
+  // clear display, prepare for next render
+  _display.clearDisplay();
+
+  int analogVal1 = analogRead(ANALOG_INPUT1_PIN);
+  int analogVal2 = analogRead(ANALOG_INPUT2_PIN);
+
+  // set new circle location based on accel
+  _x = analogVal1 / (float)MAX_ANALOG_VAL;
+  _y = analogVal2 / (float)MAX_ANALOG_VAL;
+
+  int xBall = _radius + _x * _display.width() - 2 * _radius;
+  int yBall = _radius + _y * _display.height() - 2 * _radius;
+
+  // Check boundaries
+  if(xBall - _radius < 0){
+    xBall = _radius;
+  }else if(xBall + _radius >= _display.width()){
+    xBall = _display.width() - _radius;
+  }
+
+  if(yBall - _radius < 0){
+    yBall = _radius;
+  }else if(_y + _radius >= _display.height()){
+    yBall = _display.height() - _radius;
+  }
+
+  // Display this normalized location to screen
+  _display.setCursor(0, 0);
+  _display.println("Normalized x,y:");
+  _display.print("X: ");
+  _display.println(_x, 4);
+  _display.print("Y: ");
+  _display.print(_y, 4);
+
+  // Transmit over serial
+  Serial.print(_x, 4);
+  Serial.print(", ");
+  Serial.println(_y, 4);
+
+  _display.fillCircle(xBall, yBall, _radius, SSD1306_WHITE);
+
+  // Render buffer to screen
+  _display.display();
+  
+  if(DELAY_LOOP_MS > 0){
+    delay(DELAY_LOOP_MS);
+  }
+}
