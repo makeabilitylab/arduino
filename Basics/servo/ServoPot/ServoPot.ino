4f4c220fa73a7b2e4a954ba7277ba037fa2f3c72
Jon Froehlich <jonfroehlich@gmail.com>
2021-06-03 15:58:15 -0700

    added comments

--- Basics/servo/ServoPot/ServoPot.ino
+++ Basics/servo/ServoPot/ServoPot.ino
@@ -1,7 +1,18 @@
 
-// See: https://learn.adafruit.com/adafruit-arduino-lesson-14-servo-motors
-
-// https://www.arduino.cc/reference/en/libraries/servo/
+/**
+ * Control a servo motor angle with a potentiometer input
+ * 
+ * Based on:
+ * https://learn.adafruit.com/adafruit-arduino-lesson-14-servo-motors
+ * 
+ * See the Arduino servo library:
+ * https://www.arduino.cc/reference/en/libraries/servo/
+ * 
+ * By Jon E. Froehlich
+ * @jonfroehlich
+ * http://makeabilitylab.io
+ *
+ */
 #include <Servo.h> 
 
 const int POTENTIOMETER_INPUT_PIN = A0;  
@@ -16,7 +27,12 @@ void setup()
  
 void loop() 
 { 
+  // Read pot value
   int potVal = analogRead(POTENTIOMETER_INPUT_PIN); 
-  int servoAngle = map(potVal, 0, MAX_ANALOG_VAL, 1, 179);
+
+  // Servo motor can move between 0 - 180 degrees
+  int servoAngle = map(potVal, 0, MAX_ANALOG_VAL, 0, 180);
+
+  // Set servo angle
   _servo.write(servoAngle);  
 } 
