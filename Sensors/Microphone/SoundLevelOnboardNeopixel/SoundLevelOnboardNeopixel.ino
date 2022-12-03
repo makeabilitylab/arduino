def91bb78bee4267adc3b96aee9e6f3913717287
Jon Froehlich <jonf@cs.uw.edu>
2022-12-02 20:47:54 -0800

    added peak-to-peak
    much better representation of sound

--- Sensors/Microphone/SoundLevelOnboardNeopixel/SoundLevelOnboardNeopixel.ino
+++ Sensors/Microphone/SoundLevelOnboardNeopixel/SoundLevelOnboardNeopixel.ino
@@ -17,39 +17,59 @@ const int NEOPIXEL_IDX = 0;
 // The nRF52840 has 10-bit ADC (0..1023)
 const int MAX_ANALOG_IN = 1023;
 const int MIC_INPUT_PIN = A0;
+const int MAX_MIC_LEVEL = 950;
+
+const int MIC_SAMPLE_WINDOW_MS = 50; // Sample window width in ms (50 ms = 20Hz)
 
 const unsigned long MAX_HUE_VALUE = 65535; // Hue is a 16-bit number
 const unsigned int MAX_BRIGHTNESS_VALUE = 50;
 const unsigned int MAX_SATURATION_VALUE = 255;
 const unsigned int BRIGHTNESS_VALUE = MAX_BRIGHTNESS_VALUE;
 
+unsigned long _startSampleTimeMs = -1;
+unsigned int _signalMax = 0;
+unsigned int _signalMin = MAX_ANALOG_IN;
+
 void setup() {
   pinMode(MIC_INPUT_PIN, INPUT);
 
   _onePixel.begin();             // Start the NeoPixel object
   _onePixel.clear();             // Set NeoPixel color to black (0,0,0)
-  _onePixel.setBrightness(20);   // Affects all subsequent settings
+  _onePixel.setBrightness(30);   // Affects all subsequent settings
   _onePixel.show();              // Show
+
+  _startSampleTimeMs = millis();
 }
 
 void loop() {  
 
   // Read in current sound level from microphone
-  int soundLevel = analogRead(MIC_INPUT_PIN);
+  int micLevel = analogRead(MIC_INPUT_PIN);
 
-  // Since microphone works peak-to-peak from 0V to the reference voltage (either 3.3V or 5V)
-  // we only look at half of the amplitude from MAX_ANALOG_IN/2 to MAX_ANALOG_IN
-  int hueVal = map(soundLevel, MAX_ANALOG_IN/2, MAX_ANALOG_IN, 0, MAX_HUE_VALUE);
-  int brightnessVal = map(soundLevel, MAX_ANALOG_IN/2, MAX_ANALOG_IN, 0, MAX_BRIGHTNESS_VALUE);
+  if(micLevel > _signalMax){
+    _signalMax = micLevel;
+  }else if(micLevel < _signalMin){
+    _signalMin = micLevel;
+  }
 
-  const int saturation = MAX_SATURATION_VALUE;
-  const int brightness = BRIGHTNESS_VALUE;
-  uint32_t rgbColor = _onePixel.ColorHSV(hueVal, saturation, brightness);
-  
-  // See: https://adafruit.github.io/Adafruit_NeoPixel/html/class_adafruit___neo_pixel.html
-  _onePixel.fill(rgbColor, 0, 1);
-  _onePixel.setBrightness(brightnessVal); 
-  _onePixel.show();
+  if(millis() - _startSampleTimeMs > MIC_SAMPLE_WINDOW_MS){
+    unsigned int peakToPeak = _signalMax - _signalMin;
 
-  Serial.println(soundLevel);
+    int hueVal = map(peakToPeak, 0, MAX_MIC_LEVEL, 0, MAX_HUE_VALUE);
+    int brightnessVal = map(peakToPeak,0, MAX_MIC_LEVEL, 5, MAX_BRIGHTNESS_VALUE);
+
+    const int saturation = MAX_SATURATION_VALUE;
+    const int brightness = BRIGHTNESS_VALUE;
+    uint32_t rgbColor = _onePixel.ColorHSV(hueVal, saturation, brightness);
+    
+    // See: https://adafruit.github.io/Adafruit_NeoPixel/html/class_adafruit___neo_pixel.html
+    _onePixel.fill(rgbColor, 0, 1);
+    _onePixel.setBrightness(brightnessVal); 
+    _onePixel.show();
+    // Serial.println(peakToPeak);
+
+    _signalMax = 0;
+    _signalMin = MAX_ANALOG_IN;
+    _startSampleTimeMs = millis();
+  }
 }
