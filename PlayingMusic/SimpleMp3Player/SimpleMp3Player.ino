1d9adaec65b023d9f716a36958f059a6df96eb19
Jon Froehlich <jonf@cs.uw.edu>
2022-12-10 14:20:29 -0800

    removed header to make just one simple .ino

--- PlayingMusic/SimpleMp3Player/SimpleMp3Player.ino
+++ PlayingMusic/SimpleMp3Player/SimpleMp3Player.ino
@@ -28,7 +28,54 @@
 #include <SPI.h>
 #include <SD.h> // https://www.arduino.cc/reference/en/libraries/sd/
 #include <Adafruit_VS1053.h> // https://github.com/adafruit/Adafruit_VS1053_Library/blob/master/Adafruit_VS1053.h
-#include "VS1053_Pins.h"
+
+// Specifically for use with the Adafruit Feather, the pins are pre-set here!
+
+// These are the pins used
+#define VS1053_RESET   -1     // VS1053 reset pin (not used!)
+
+// Feather ESP8266
+#if defined(ESP8266)
+  #define VS1053_CS      16     // VS1053 chip select pin (output)
+  #define VS1053_DCS     15     // VS1053 Data/command select pin (output)
+  #define CARDCS          2     // Card chip select pin
+  #define VS1053_DREQ     0     // VS1053 Data request, ideally an Interrupt pin
+
+// Feather ESP32
+#elif defined(ESP32) && !defined(ARDUINO_ADAFRUIT_FEATHER_ESP32S2)
+  #define VS1053_CS      32     // VS1053 chip select pin (output)
+  #define VS1053_DCS     33     // VS1053 Data/command select pin (output)
+  #define CARDCS         14     // Card chip select pin
+  #define VS1053_DREQ    15     // VS1053 Data request, ideally an Interrupt pin
+
+// Feather Teensy3
+#elif defined(TEENSYDUINO)
+  #define VS1053_CS       3     // VS1053 chip select pin (output)
+  #define VS1053_DCS     10     // VS1053 Data/command select pin (output)
+  #define CARDCS          8     // Card chip select pin
+  #define VS1053_DREQ     4     // VS1053 Data request, ideally an Interrupt pin
+
+// WICED feather
+#elif defined(ARDUINO_STM32_FEATHER)
+  #define VS1053_CS       PC7     // VS1053 chip select pin (output)
+  #define VS1053_DCS      PB4     // VS1053 Data/command select pin (output)
+  #define CARDCS          PC5     // Card chip select pin
+  #define VS1053_DREQ     PA15    // VS1053 Data request, ideally an Interrupt pin
+
+#elif defined(ARDUINO_NRF52832_FEATHER )
+  #define VS1053_CS       30     // VS1053 chip select pin (output)
+  #define VS1053_DCS      11     // VS1053 Data/command select pin (output)
+  #define CARDCS          27     // Card chip select pin
+  #define VS1053_DREQ     31     // VS1053 Data request, ideally an Interrupt pin
+
+// Feather M4, M0, 328, ESP32S2, nRF52840 or 32u4
+#else
+  #define VS1053_CS       6     // VS1053 chip select pin (output)
+  #define VS1053_DCS     10     // VS1053 Data/command select pin (output)
+  #define CARDCS          5     // Card chip select pin
+  // DREQ should be an Int pin *if possible* (not possible on 32u4)
+  #define VS1053_DREQ     9     // VS1053 Data request, ideally an Interrupt pin
+#endif
 
 Adafruit_VS1053_FilePlayer _musicPlayer = 
   Adafruit_VS1053_FilePlayer(VS1053_RESET, VS1053_CS, VS1053_DCS, VS1053_DREQ, CARDCS);
@@ -291,6 +338,7 @@ void startPlayingSound(int curSoundFileIndex){
   // delay(50);
 
   Serial.println("The next song to play is " + _soundFiles[curSoundFileIndex] + " at index " + curSoundFileIndex);
+  Serial.println("Calling _musicPlayer.startPlayingFile...")
   delay(50);
 
   // Sometimes we crash right here!
