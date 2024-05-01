5f02949e7d5215b0240737af8ced1c642bdfffcd
Jon Froehlich <jonf@cs.uw.edu>
2024-05-01 16:05:51 -0700

    Added simple spacebar button example

---
+++ HumanInterfaceDevice/SpaceBarButton/SpaceBarButton.ino
@@ -0,0 +1,39 @@
+
+/**
+ * Basic button as a space bar example. Press button, sends a SPACE BAR
+ * via Arduino keyboard emulation.
+ *
+ * Tutorials here: https://makeabilitylab.github.io/physcomp
+ *   
+ * By Jon E. Froehlich
+ * @jonfroehlich
+ * http://makeabilitylab.io
+ *
+ */
+
+ #include <Keyboard.h> // https://www.arduino.cc/reference/en/language/functions/usb/keyboard/
+
+                                                                
+const int BUTTON_INPUT_PIN = 2;
+bool _btnPressed = false;
+
+void setup() {
+  Serial.begin(9600); // for debugging
+  Keyboard.begin();
+  pinMode(BUTTON_INPUT_PIN, INPUT_PULLUP);
+}
+
+void loop() {
+  int btnVal = digitalRead(BUTTON_INPUT_PIN);
+  if(btnVal == LOW){ // internal-pull configuration
+    _btnPressed = true;
+    Keyboard.press(' ');
+    Serial.println("Spacebar PRESSED!");
+  }else{
+    if(_btnPressed){
+      _btnPressed = false;
+      Keyboard.release(' ');
+      Serial.println("Spacebar RELEASED!");
+    }
+  }
+}
