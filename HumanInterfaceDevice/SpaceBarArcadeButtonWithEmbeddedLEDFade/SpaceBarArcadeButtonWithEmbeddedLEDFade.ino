e945dc9a24e893a706f95259ee77da867e209646
Jon Froehlich <jonf@cs.uw.edu>
2024-05-02 07:59:17 -0700

    separated out the embedded led fade with simpler example

--- HumanInterfaceDevice/SpaceBarArcadeButtonWithEmbeddedLEDFade/SpaceBarArcadeButtonWithEmbeddedLEDFade.ino
+++ HumanInterfaceDevice/SpaceBarArcadeButtonWithEmbeddedLEDFade/SpaceBarArcadeButtonWithEmbeddedLEDFade.ino
@@ -3,7 +3,7 @@
  * Basic arcade button as a space bar example. Press arcade button, sends a SPACE BAR
  * via Arduino keyboard emulation.
  *
- * See also: SpaceBarArcadeButtonWithEmbeddedLEDFade.ino
+ * The embedded LED fades on/off to attract users! :)
  *
  * Should work with any button with an embedded LED, such as:
  * - Arcade Button with LED - 30mm: https://www.adafruit.com/product/3489
@@ -20,9 +20,17 @@
 
  #include <Keyboard.h> // https://www.arduino.cc/reference/en/language/functions/usb/keyboard/
 
+// Two values change the LED fade speed:
+// - The time between fade steps (in milliseconds below)
+// - The fade step itself
+const int TIME_BETWEEN_FADE_STEP_IN_MS = 8; // change this to change fade speed
+
 const int ARCADE_BUTTON_INPUT_PIN = 2;
 const int ARCADE_BUTTON_LED_PIN = 3; // needs to be a PWM pin
 const int MAX_FADE_VAL = 100; // should be less than max PWM output
+int _curFadeVal = 0;
+int _fadeStep = 1; // change this to quicken fade (but will be less smooth so limit to max ~20)
+unsigned long _lastFadeTimestampMs = 0;
 bool _btnPressed = false;
 
 void setup() {
@@ -30,6 +38,8 @@ void setup() {
   Keyboard.begin();
   pinMode(ARCADE_BUTTON_INPUT_PIN, INPUT_PULLUP);
   pinMode(ARCADE_BUTTON_LED_PIN, OUTPUT);
+  analogWrite(ARCADE_BUTTON_LED_PIN, _curFadeVal);
+  _lastFadeTimestampMs = millis();
 }
 
 void loop() {
@@ -39,14 +49,30 @@ void loop() {
     _btnPressed = true;
     Keyboard.press(' ');
     Serial.println("Spacebar PRESSED!");
+
+    // reset fade val to lowest so LED turns off when button released
+    _curFadeVal = 0;
+    if(_fadeStep < 0){
+      _fadeStep *= -1;
+    }
   }else{
     if(_btnPressed){
       _btnPressed = false;
-      digitalWrite(ARCADE_BUTTON_LED_PIN, LOW); // turn on embedded LED
       Keyboard.release(' ');
       Serial.println("Spacebar RELEASED!");
     }
-  }
 
-  delay(20); // Slight delay, sampling at 50 Hz
+    // If we are here, the button is not pressed, so continue fade loop
+    // Check to see if enough time has passed to fade since last fade step
+    if(millis() - _lastFadeTimestampMs > TIME_BETWEEN_FADE_STEP_IN_MS){
+      analogWrite(ARCADE_BUTTON_LED_PIN, _curFadeVal);
+      _lastFadeTimestampMs = millis();
+
+      _curFadeVal += _fadeStep;
+      _curFadeVal = constrain(_curFadeVal, 0, MAX_FADE_VAL);
+      if(_curFadeVal <= 0 || _curFadeVal >= MAX_FADE_VAL){
+        _fadeStep *= -1;
+      }     
+    }
+  }
 }
